a3259052dd64ebdab3f75046735c8c978376e7d7
Etienne Georgy <egeorgy@hsr.ch>
2015-09-21 17:14:33 +0200

    solved excercise 1.5 and 1.6

--- 1.5_Identifiers_Types/Program.cs
+++ 1.5_Identifiers_Types/Program.cs
@@ -15,10 +15,13 @@ namespace _1._5_Identifiers_Types
         private static void TestSymbols()
         {
             // TODO: Erstellen Sie eine int-Variable mit dem Namen "null"
+            int Null;
 
             // TODO: Weisen Sie der Variable den Wert 1 zu
-
+            Null = 1;
+            
             // TODO: Geben Sie den Wert auf die Konsole aus
+            System.Console.WriteLine(Null);
         }
 
         private static void TestPrimitiveTypes()
@@ -27,10 +30,13 @@ namespace _1._5_Identifiers_Types
             int i = 255;
 
             // TODO: Weisen Sie "c" einer neuen decimal-Variable "d" zu
+            decimal d = c;
 
             // TODO: Weisen Sie "i" einer neuen sbyte-Variable "s" zu
-
+            sbyte s = (sbyte) i;
+            
             // TODO: Geben Sie den Wert auf die Konsole aus
+            System.Console.WriteLine(c + " " + i);
 
             // TODO: Optional / Überlegen Sie sich, wieso das gegebene Resultat entsteht (Hinweis: Bitlogik!)
 
--- 1.6_Statements/Program.cs
+++ 1.6_Statements/Program.cs
@@ -6,16 +6,43 @@ namespace _1._6_Statements
     {
         static void Main(string[] args)
         {
-            // TODO: PromptHeight() verwenden, um Höhe des Baumes (ohne Stamm) einzulesen
+            // PromptHeight() verwenden, um Höhe des Baumes (ohne Stamm) einzulesen
+            int height = PromptHeight();
+            
+            // Ungültige Eingabe behandeln
+            if (height == 0)
+            {
+                System.Console.WriteLine("Programm wird beendet");
+            }
 
-            // TODO: Ungültige Eingabe behandeln
+            // Nadelwerk zeichnen
+            else
+            {
+                for (int i = height; i > 0; i--)
+                {
+                    for (int j = i; j <= height; j++)
+                    {
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                }
 
-            // TODO: Nadelwerk zeichnen
 
-            // TODO: Stamm zeichnen
+                // Stamm zeichnen
+                int trunkWidth = height / 3;
+                int trunkHeight = height / 2;
+                for (int i = trunkHeight; i > 0; i--)
+                {
+                    for (int j = 0; j < trunkWidth; j++)
+                    {
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                }
+            }
 
             // TODO: Loop implementieren
-
+           
             Console.ReadKey();
         }
 

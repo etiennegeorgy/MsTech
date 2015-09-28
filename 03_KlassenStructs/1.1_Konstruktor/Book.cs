d747ea287b00ca238624045594e77544ab5367fd
Etienne Georgy <egeorgy@hsr.ch>
2015-09-28 15:17:10 +0200

    solved excercise 2.1

--- 03_KlassenStructs/1.1_Konstruktor/Book.cs
+++ 03_KlassenStructs/1.1_Konstruktor/Book.cs
@@ -12,17 +12,32 @@ namespace _1._1_Konstruktor
         {
             {
                 // TODO: Felder initialisieren
+                this.title = title;
+                this.author = author;
+                this.available = available;
             }
         }
 
         // TODO: Konstruktor mit title / author
-
+        public Book(string title, string author) : this(title, author, true)
+        {
+            
+        }
         // TODO: Konstruktor mit title
+        public Book(string title) : this(title, "anonymous", false)
+        {
 
+        }
         // TODO: Default-Konstruktor
+        public Book() : this("untitled", "anonymous", false)
+        {
 
+        }
         // TODO: ToString()-Methode
-
+        public override string ToString()
+        {
+            return title + ", " + author + ", " + available;
+        }
     }
 
 }
--- 03_KlassenStructs/1.1_Konstruktor/Program.cs
+++ 03_KlassenStructs/1.1_Konstruktor/Program.cs
@@ -6,15 +6,15 @@ namespace _1._1_Konstruktor
     {
         static void Main(string[] args)
         {
-            //Book b1 = new Book("The Green Mile", "Stephen King", false);
-            //Book b2 = new Book("It", "Stephen King");
-            //Book b3 = new Book("Misery");
-            //Book b4 = new Book();
+            Book b1 = new Book("The Green Mile", "Stephen King", false);
+            Book b2 = new Book("It", "Stephen King");
+            Book b3 = new Book("Misery");
+            Book b4 = new Book();
 
-            //Console.WriteLine(b1);
-            //Console.WriteLine(b2);
-            //Console.WriteLine(b3);
-            //Console.WriteLine(b4);
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
+            Console.WriteLine(b4);
 
             Console.ReadKey();
         }

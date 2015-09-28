a44d5555f19d6244a09b70d6cc2a38702075bd44
Etienne Georgy <egeorgy@hsr.ch>
2015-09-28 20:44:57 +0200

    added excersice 2.5

---
+++ 03_KlassenStructs/1.5_Properties/Author.cs
@@ -0,0 +1,26 @@
+﻿namespace _1._5_Properties
+{
+    class Author
+    {
+        // HINWEIS: Versuchen Sie so wenige Fields wie möglich zu verwenden!
+
+        // TODO: Konstruktur der den Vornamen und Nachnahmen des Autors entgegennimmt.
+        
+        
+        // TODO: Default-Konstruktor (stellt einen anonymen Autor dar).
+        
+
+        // TODO: Readonly Property für Vornamen.
+
+        // TODO: Readonly Property für Nachnamen.
+
+        // TODO: Readonly Property für den vollen Namen (Vor- und Nachnamen kombiniert), Formatierung ist egal
+        // Handelt es sich um einen anonymen Autor, soll "anonymous" ausgegeben werden.
+        
+
+        // TODO: Property um festzustellen, ob der Autor anonym sein soll
+        // Wurde der Default-Konstruktur aufgerufen, ist der Autor immer anonym
+        // und der Wert darf nicht verändert werden.
+        
+    }
+}
---
+++ 03_KlassenStructs/1.5_Properties/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _1._5_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test mit einem Autoren
+            
+            // TODO: uncleBob-Variable verwenden
+            //var uncleBob = new Author("Robert C.", "Martin");
+
+            Header("Uncle Bob");
+
+            // TODO: Geben Sie den Namen des Autors (uncleBob) mit den Properties für Vor- und Nachnamen aus.
+
+            // TODO: Geben Sie den vollen Namen des Autors mit einem Property aus.
+
+            // TODO: Geben sie den Wert des "Ist anonym"-Property aus.
+            // Der Autor sollte hier nicht anonym sein.
+
+            // TODO: Ändern Sie den Wert des "Ist anonym"-Properties, dass der Autor nun als anonym behandelt wird.
+
+            // TODO: Geben Sie erneut den Wert des "Ist anonym"-Properties aus.
+            // Der Autor sollte nun als anonym gelten. 
+
+            // TODO: Geben Sie den vollen Namen des Autors aus.
+            // Er sollte nun "anonymous" heissen.
+
+            Console.WriteLine();
+
+
+            // Test mit einem anonymen Autoren
+            var anonymousAuthor = new Author();
+
+            Header("Anonymous Author");
+
+            // TODO: Geben Sie den vollen Namen des anonymen Autors (anonymousAuthor) mit einem Property aus.
+
+            // TODO: Geben Sie den Wert des "Ist anonym"-Properties aus.
+            // Der Autor sollte anonym sein.
+
+            // TODO: Ändern Sie den Status des "Ist anonym"-Properties, sodass der Autor nicht mehr anonym wäre.
+
+            // TODO: Geben Sie erneut den Wert des "Ist anonym"-Properties aus.
+            // Der Autor sollte nun immernoch anonym sein. 
+
+            Console.ReadKey();
+        }
+
+        private static void Header(string author)
+        {
+            var header = string.Format("Testing: {0}", author);
+            Console.WriteLine(header);
+            Console.WriteLine(new string('#', header.Length));
+        }
+    }
+}

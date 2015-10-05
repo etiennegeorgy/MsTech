65dff7b30c8898ae4f3eb6b2afeebb791dadc028
Etienne Georgy <egeorgy@hsr.ch>
2015-10-05 15:35:17 +0200

    solved excercise 4.1

--- 04_Vererbung/1.1_Shapes/Program.cs
+++ 04_Vererbung/1.1_Shapes/Program.cs
@@ -7,12 +7,19 @@ namespace _1._1_Shapes
         static void Main(string[] args)
         { 
             // TODO: Erzeuge Square-Objekt
+            Square s = new Square(1, 2, 4);
 
             // TODO: Ausgabe Degrees
-
+            System.Console.WriteLine(s.Degrees);
+            
             // TODO: Ausgabe Area()
+            System.Console.WriteLine(s.Area());
 
             // TODO: Ausgabe X1, Y1, X2, Y2
+            System.Console.WriteLine(s.X1);
+            System.Console.WriteLine(s.Y1);
+            System.Console.WriteLine(s.X2);
+            System.Console.WriteLine(s.Y2);
 
             Console.ReadKey();
         }
--- 04_Vererbung/1.1_Shapes/Square.cs
+++ 04_Vererbung/1.1_Shapes/Square.cs
@@ -2,16 +2,57 @@ using System;
 
 namespace _1._1_Shapes
 {
-    class Square // TODO: Von Klasse Polygon ableiten
+    // TODO: Von Klasse Polygon ableiten
+    class Square : Polygon
     {
         // TODO: Felder implementieren
+        private float x1;
+        private float y1;
+        private float len;
+        private const ushort numSides = 4;
 
         // TODO: Konstruktor implementieren
+        public Square(float x1, float y1, float len)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.len = len;
+        }
 
         // TODO: Basis-Members implementieren
+        public override ushort NumSides
+        {
+	        get { return numSides; }
+        }
+
+        public override float Area()
+        {
+ 	        return len * len;
+        }
 
         // TODO: X1 / X2 implementieren
+        public float X1
+        {
+            get { return x1; }
+            set { x1 = value; }
+        }
+
+        public float X2
+	    {
+		    get { return x1 + len; }
+	    }
 
         // TODO: Y1 / Y2 implementieren
+        public float Y1
+        {
+            get { return y1; }
+            set { y1 = value; }
+        }
+
+	    public float Y2
+	    {
+		    get { return y1 + len; }
+	    }
+	
     }
 }

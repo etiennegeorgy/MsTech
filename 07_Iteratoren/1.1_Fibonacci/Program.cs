496acadebce85b1e894fa2f58d7b220bccd8196c
Etienne Georgy <egeorgy@hsr.ch>
2015-10-26 16:30:07 +0100

    solved excercise 7.1.1

--- 07_Iteratoren/1.1_Fibonacci/Program.cs
+++ 07_Iteratoren/1.1_Fibonacci/Program.cs
@@ -6,17 +6,30 @@ namespace _1._1_Fibonacci
     public class Math
     {
         // TODO: Implementiere Methode Fibonacci
-        
+        public static IEnumerable<int> Fibonacci(int value)
+        {
+            int prev = 0;
+            int next = 1;
+            int sum = 1;
+            for (int i = 0; i < value; i++)
+            {
+                yield return sum;
+                sum = prev + next;
+                prev = next;
+                next = sum;
+                
+            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            //foreach (int i in Math.Fibonacci(8))
-            //{
-            //    Console.WriteLine("{0} ", i);
-            //}
+            foreach (int i in Math.Fibonacci(8))
+            {
+                Console.WriteLine("{0} ", i);
+            }
 
             Console.ReadKey();
         }
